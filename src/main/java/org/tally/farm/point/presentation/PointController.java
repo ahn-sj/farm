0aabc30e325364b4dbca569b80b9c5f08197d5ae
ahn-sj <jaesa5221@gmail.com>
2023-12-25 00:21:39 +0900

    feat: OpenAPI Spec Swagger 추가

--- src/main/java/org/tally/farm/point/presentation/PointController.java
+++ src/main/java/org/tally/farm/point/presentation/PointController.java
@@ -1,5 +1,6 @@
 package org.tally.farm.point.presentation;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,15 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.tally.farm.point.application.PointService;
 
-import static org.tally.farm.point.dto.PointRequest.*;
+import static org.tally.farm.point.dto.PointRequest.PointCreate;
 
+@Tag(name = "01. POINT", description = "POINT API")
 @RestController
 @RequiredArgsConstructor
 public class PointController {
 
     private final PointService pointService;
 
-    @PostMapping("/v1/point-charge")
+    @PostMapping("/v1/points/point-charge")
     public ResponseEntity<Void> insertPoint(@RequestBody PointCreate request) {
         pointService.earnPoint(request);
         return ResponseEntity.ok().build();
